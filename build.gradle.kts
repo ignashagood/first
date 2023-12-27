e108347200cb84c2c46ede3c8ca24849efe2d1fc
user <ignatik2186@gmail.com>
2023-12-27 19:40:01 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(platform("org.http4k:http4k-bom:5.8.3.0"))
+    implementation("org.http4k:http4k-core")
+    implementation("org.http4k:http4k-server-undertow")
+    implementation("org.http4k:http4k-client-apache")
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("TicketsAppKt")
+}
